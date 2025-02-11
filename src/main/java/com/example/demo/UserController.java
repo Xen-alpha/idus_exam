c9504c61363e3bbd810749652c21e788cbaf44f2
Xen-alpha <hky7548@naver.com>
2025-02-11 17:36:50 +0900

    [Docs] swagger 설명 추가

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.example.demo.model.common.BaseResponse;
 import com.example.demo.model.user.*;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
+    @Operation(summary="회원 가입", description="회원 정보로 이메일, 실명, 닉네임, 휴대폰 번호, 이메일, 성별을 수집합니다.")
     public ResponseEntity<BaseResponse> signup(@RequestBody SignupRequest request) {
         System.out.println("Signup request detected");
         BaseResponse response = new BaseResponse();
@@ -22,7 +24,9 @@ public class UserController {
         response.setSuccess();
         return ResponseEntity.ok(response);
     }
+    
     @GetMapping("/info/{idx}")
+    @Operation(summary="유저 정보 보기", description="DB 내 특정 인덱스의 이메일, 실명, 닉네임, 휴대폰 번호, 이메일, 성별을 보여줍니다.")
     public ResponseEntity<UserDetailResponse> getUserInfo(@PathVariable Long idx) {
         UserDetailResponse response = new UserDetailResponse();
         UserDetailDto dto = userService.getUserDetail(idx);
@@ -37,6 +41,7 @@ public class UserController {
         return ResponseEntity.ok(response);
     }
     @GetMapping("/order/{idx}")
+    @Operation(summary="회원 주문 정보 조회", description="DB 내 특정 인덱스 회원이 주문한 물품 목록을 보여줍니다.")
     public ResponseEntity<UserOrderResponse> getUserOrder(@PathVariable Long idx) {
         UserOrderResponse response = new UserOrderResponse();
         List<OrderDto> dtos = userService.getUserOrder(idx);
@@ -50,6 +55,7 @@ public class UserController {
     }
 
     @GetMapping("/list")
+    @Operation(summary="전체 회원 조회", description="전체 회원 목록과 각 회원 별 가장 최근 주문 정보를 보여줍니다.")
     public ResponseEntity<UserListResponse> getUserList(String name, String email, Integer page) {
         UserListResponse response = new UserListResponse();
         List<UserListDto> dtos = userService.getUserList(email, name, page);
--- src/main/java/com/example/demo/model/user/OrderDto.java
+++ src/main/java/com/example/demo/model/user/OrderDto.java
@@ -1,5 +1,7 @@
 package com.example.demo.model.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.SchemaProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +14,10 @@ import java.sql.Date;
 @Getter
 @Setter
 public class OrderDto {
+    @Schema(name="주문번호")
     private String orderId;
+    @Schema(name="주문 상품")
     private String productName;
+    @Schema(name="주문 일자")
     private Date datetime;
 }
